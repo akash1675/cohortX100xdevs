61f8f910712a3bbe5040c4afc13d7125f60b75d1
akash1675 <captainakash1675@gmail.com>
2024-10-13 21:45:32 +0530

    add week-9.2

--- week-9.2/week-9-react-practice/src/App.jsx
+++ week-9.2/week-9-react-practice/src/App.jsx
@@ -1,11 +1,83 @@
 import { useState, useEffect } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  function increase(){
+    setCount(cnt=>cnt+1);
+  }
+
+  function decrease(){
+    setCount2(cnt => cnt-1);
+  }
+
+  return (
+    <>
+      <div>
+        <Counter count={count} count2={count2} />
+        <button onClick={increase}>Increase count</button>
+        <button onClick={decrease}>decrease count</button>
+      </div>
+    </>
+  )
+
+}
+
+// mouting ,re-rendering, unmounting
+function Counter(props) {
+
+  useEffect(function () {
+    console.log("mount");
+
+    return function () {
+      console.log("unmount");
+    }
+  }, [])
+
+  useEffect(function(){
+    console.log("count has changed");
+    return function(){
+      console.log("cleanup inside second effect")
+    }
+  }, [props.count])
+
+
+  return (
+    <>
+      <div>counter1 {props.count}</div>
+      <div>counter2 {props.count2}</div>
+    </>
+  )
+
+}
+
+export default App
+
+
+
+
+
+// creating counter button or mounting, unmounting, using useEffect
+/*
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  let [counterVisible, setCounterVisible] = useState(true);
+
+  useEffect(function(){
+    setInterval(function(){
+      setCounterVisible(counterVisible => !counterVisible)
+    }, 5000);
+  }, [])
+
   return (
     <>
-    <Counter></Counter>
+    hi
+    {counterVisible && <Counter></Counter>}
+    hello
     </>
   )
 
@@ -17,14 +89,24 @@ function Counter() {
 
 
   useEffect(function(){
-    setInterval(function(){
+    console.log("on mount")
+    let clock = setInterval(function(){
+      console.log("from inside set-Interval");
       // setCount(count=>count+1);
       setCount(function(count){
         return count+1;
       })
-    }, 60000)
-    console.log("mounted");
-  }, []);
+    }, 1000)
+     
+    //clearInterval
+    return function(){
+      console.log("un mount")
+      clearInterval(clock);
+    }
+    
+  }, []); // dependency array, cleanup, fetch inside useEffect
+
+
 
 
   function increseCount(){
@@ -52,3 +134,5 @@ function Counter() {
 }
 
 export default App
+
+*/
