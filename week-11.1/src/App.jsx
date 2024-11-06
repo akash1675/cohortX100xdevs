52da377abf494abee08539b51dffdfeb39c10fcd
akash1675 <captainakash1675@gmail.com>
2024-11-06 22:31:05 +0530

    adding hooks

--- week-11.1/src/App.jsx
+++ week-11.1/src/App.jsx
@@ -1,10 +1,33 @@
-import { usePostTitle } from "./hooks/useFetch"
+import { useFetch } from "./hooks/useFetch"
+// import { usePostTitle } from "./hooks/useFetch"
+import { useState } from "react";
 
 
 function App() {
-  const postTitle = usePostTitle();
+  const [currentPost, setCurrentPost] = useState(1);
+
+  const { finalData, loding } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+
+  // const postTitle = usePostTitle();
+
+  if (loding) {
+    return <div>
+      Loding.....
+    </div>
+  }
+
   return (
-    <div>{postTitle}</div>
+    <>
+
+      <button onClick={() => setCurrentPost(1)}>1</button>
+      <button onClick={() => setCurrentPost(2)}>2</button>
+      <button onClick={() => setCurrentPost(3)}>3</button>
+      <div>
+        {/* <div>{postTitle}</div> */}
+        {JSON.stringify(finalData)}
+      </div>
+    </>
+
   )
 }
 
--- week-11.1/src/hooks/useFetch.js
+++ week-11.1/src/hooks/useFetch.js
@@ -1,17 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 
-export function usePostTitle(){
-   const [post, setPost] = useState({});
+// export function usePostTitle(){
+//    const [post, setPost] = useState({});
 
-   async function  getPost() {
-      const resp = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-      const json = await resp.json();
-      setPost(json);
+//    async function  getPost() {
+//       const resp = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//       const json = await resp.json();
+//       setPost(json);
+//    }
+
+//    useEffect(() =>{
+//       getPost();
+//    }, [])
+
+//    return post.title;
+// }
+
+export function useFetch(url){
+   const [finalData, setFinalData] = useState({});
+   const [loding, setLoding] = useState(true);
+   
+   async function getdetails() {
+      setLoding(true);
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      setFinalData(jsonData);
+      setLoding(false)
    }
 
-   useEffect(() =>{
-      getPost();
-   }, [])
+   useEffect(()=>{
+      getdetails();
+   }, [url])
 
-   return post.title;
+   return {finalData,loding}
 }
