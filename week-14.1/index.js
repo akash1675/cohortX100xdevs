2028814484135d6e71c174904ff2babbfa626a7d
akash1675 <captainakash1675@gmail.com>
2024-11-09 20:55:01 +0530

    add ts file

---
+++ week-14.1/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// let x: number = 1;
+// console.log(x);
+function func(ans) {
+    console.log("hello" + ans);
+}
+// let ans: any = "rahul"
+// funct(ans);
+// func("rahul");
+// func("akash");
+
+function sum(a, b) {
+    return a + b;
+}
+let ans = sum(1, 2);
+console.log(ans);
---
+++ week-14.1/index.ts
@@ -0,0 +1,27 @@
+// let x: number = 1;
+// console.log(x);
+
+
+function func(ans:any, ){
+   console.log("hello"+ans);
+   
+}
+// let ans: any = "rahul"
+// funct(ans);
+
+// func("rahul");
+// func("akash");
+
+
+
+function sum(a:number, b:number){
+   return a+b;
+}
+
+// let ans = sum(1, 2);
+// console.log(ans);
+
+
+
+
+
